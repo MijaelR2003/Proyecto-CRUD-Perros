ea74d85c93913bd81622f7c70e306256286eac7d
Mijael Rojas <rojasmijael67@gmail.com>
2024-06-24 22:56:03 -0400

    Se puede ver perros

---
+++ src/pages/Home.jsx
@@ -0,0 +1,40 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+
+const apiUrl = "https://experimental-dogs-experimental-org.koyeb.app/api/v1/dogs"
+
+function Home() {
+    const [perros, setPerros] = useState([])
+
+    useEffect(()=>{
+        axios.get(apiUrl)
+        .then((res)=>{
+            setPerros(res.data)
+        }).catch((error)=>{
+            console.log(error)
+        })
+    })
+
+    const nuevoArreglo = perros.map((perro)=>{
+        return (
+            <li key={perro._id}>
+                <p>{perro.name}</p>
+                <p>{perro.breed}</p>
+                <p>{perro.color}</p>
+                <p>{perro.age}</p>
+                <p>{perro.favoriteToy}</p>
+                <p>{perro.personality}</p>
+                <p>{perro.bio}</p>
+            </li>
+        )
+    })
+
+  return (
+    <div>
+        <ul>{nuevoArreglo}</ul>
+    </div>
+  )
+}
+
+export default Home
