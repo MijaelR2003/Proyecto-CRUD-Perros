a36d11d419b4f9db8ca15b6ab519d7d83e463e9d
Mijael Rojas <rojasmijael67@gmail.com>
2024-06-28 19:36:13 -0400

    Modificacion de la Navegacion

--- src/Rutas.jsx
+++ src/Rutas.jsx
@@ -1,24 +1,27 @@
-import React from 'react'
+import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ObtenerPerro from "./pages/ObtenerPerro";
 import ActualizarPerro from "./pages/ActualizarPerro";
 import Nave from "./pages/Nave";
-import CrearPerro from './pages/CrearPerro';
+import CrearPerro from "./pages/CrearPerro";
 
 function Rutas() {
   //const apiUrl =
   //"https://experimental-dogs-experimental-org.koyeb.app/api/v1/dogs";
   const apiUrl =
-  "https:experimental-unicorns-experimental-org.koyeb.app/api/v1/unicorns";
+    "https:experimental-unicorns-experimental-org.koyeb.app/api/v1/unicorns";
 
   return (
     <>
-      <Nave />
       <BrowserRouter>
+        <Nave />
         <Routes>
-          <Route path="/" element={<ObtenerPerro apiUrl={apiUrl}/>} />
-          <Route path='/crear-perro' element={<CrearPerro apiUrl={apiUrl}/>}/>
-          <Route path="/actualizar-perro/:id" element={<ActualizarPerro apiUrl={apiUrl}/>} />
+          <Route path="/" element={<ObtenerPerro apiUrl={apiUrl} />} />
+          <Route path="/crear-perro" element={<CrearPerro apiUrl={apiUrl} />} />
+          <Route
+            path="/actualizar-perro/:id"
+            element={<ActualizarPerro apiUrl={apiUrl} />}
+          />
         </Routes>
       </BrowserRouter>
     </>
--- src/pages/Nave.jsx
+++ src/pages/Nave.jsx
@@ -1,16 +1,31 @@
 import React from 'react'
 import { Container, Nav, Navbar } from 'react-bootstrap'
+import { useNavigate } from 'react-router-dom'
 
 function Nave() {
+  const navigate = useNavigate()
+
+  const volver = ()=>{
+    navigate(-1)
+  }
+
+  const home = ()=>{
+    navigate("/")
+  }
+
+  const ver = ()=>{
+    navigate(-1)
+  }
+  
   return (
     <Navbar bg="dark" data-bs-theme="dark" className="mb-3">
       <Container>
-        <Navbar.Brand href="/">Aplicacion de Perros</Navbar.Brand>
+        <Navbar.Brand onClick={home}>Aplicacion de Perros</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/crear-perro">Crear Perros</Nav.Link>
-            <Nav.Link href="/">Link</Nav.Link>
+            <Nav.Link onClick={ver}>Crear Perros</Nav.Link>
+            <Nav.Link onClick={volver}>Volver</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
